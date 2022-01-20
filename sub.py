c06d81276c877ac5a0f4e8eb2d3b8fe9aea4fb2b
shadow-securities <68288503+shadow-securities@users.noreply.github.com>
2022-01-20 12:11:17 +0000

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,3 +1,27 @@
+
+cyan="\033[1;36;40m"
+green="\033[1;32;40m"
+red="\033[1;31;40m"
+Y = '\033[1;33;40m' 
+
+        
+
+if __name__=="__main__":
+    print(cyan+"""
+    
+ .d8888. db    db d8888b.   d88888b d8b   db db    db .88b  d88. 
+88'  YP 88    88 88  `8D   88'     888o  88 88    88 88'YbdP`88 
+`8bo.   88    88 88oooY'   88ooooo 88V8o 88 88    88 88  88  88 
+  `Y8b. 88    88 88~~~b.   88~~~~~ 88 V8o88 88    88 88  88  88 
+db   8D 88b  d88 88   8D   88.     88  V888 88b  d88 88  88  88 
+`8888Y' ~Y8888P' Y8888P'   Y88888P VP   V8P ~Y8888P' YP  YP  YP 
+                                                                
+                                                                
+    """)
+    print(Y+" Created By : JAYASURYAPAL G")
+    print(Y+" Github profile: https://github.com/shadow-securities")
+
+
 import requests
 import json
 R = '\033[31m' 
